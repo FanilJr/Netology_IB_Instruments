decf84496fd66e918788f569dd81a6b74652d78e
Fanil_Jr <ethic91@icloud.com>
2022-05-20 12:27:30 +0500

    yulka

---
+++ Navigation/Data/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Fanil_Jr on 18.05.2022.
+//
+
+import Foundation
+
+struct Post {
+    
+    var title: String
+    
+}
---
+++ Navigation/UITabBar.swift
@@ -0,0 +1,20 @@
+//
+//  UITabBar.swift
+//  Navigation
+//
+//  Created by Fanil_Jr on 19.05.2022.
+//
+
+import Foundation
+import UIKit
+
+// MARK: решил добавить прозрачости табБару
+
+extension UITabBar {
+    
+    static func setTransparentTabbar() {
+        UITabBar.appearance().backgroundImage = UIImage()
+        UITabBar.appearance().shadowImage     = UIImage()
+        UITabBar.appearance().clipsToBounds   = true
+    }
+}
